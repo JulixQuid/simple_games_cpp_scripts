33a66b021bac9bb7c417cfab844566a097d20bc0
Julian Angulo <julianangulo@Julians-MacBook-Pro.local>
2022-02-25 08:11:50 -0500

    fix: Added print statements

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 int main()
 {
-    std::cout << "Hello World!";
+    std::cout << "You are a secret agent breaking into a secure server room";
+    std::cout << std::endl;
+    std::cout << "Enter the correct code to continue...";
     return 0;
 }
