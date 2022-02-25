c6abf3ea4f4d16c6b0942b9bd54fcca836baa9aa
Julian Angulo <julianangulo@Julians-MacBook-Pro.local>
2022-02-25 08:59:00 -0500

    refactor: Renamed varibales using upper camel case for UE4 compatibility

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 int main()
 {
+    // Print a welcome message
     std::cout << "You are a secret agent breaking into a secure server room";
     std::cout << std::endl;
     std::cout << "Enter the correct code to continue...";
 
-    int a = 4;
-    int b = 2;
-    int c = 3;
+    // Declare 3 number code
+    const int CodeA = 4; // prtoecting a from changing in the futura
+    const int CodeB = 2;
+    const int CodeC = 3;
 
-    int sum = a + b + c;
-    int mult = a * b * c;
+    // Operate the variables
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+    // Print sum and product to the terminal
     std::cout << std::endl;
-    std::cout << sum << std::endl;
-    std::cout << mult;
+    std::cout << "There are 3 numbers un the code" << std::endl;
+    std::cout << "The codes add-up to: " << CodeSum << std::endl;
+    std::cout << "The codes muliply to: "<< CodeProduct;
+
+    int PlayerGuess;
+
     return 0;
 
 }
