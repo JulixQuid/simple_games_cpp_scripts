2daf60c44ecb87997b5e315046584fbeb4bbd75b
Julian Angulo <julianangulo@Julians-MacBook-Pro.local>
2022-02-25 08:21:19 -0500

    feat: declared variables

--- TripleX.cpp
+++ TripleX.cpp
@@ -4,5 +4,16 @@ int main()
     std::cout << "You are a secret agent breaking into a secure server room";
     std::cout << std::endl;
     std::cout << "Enter the correct code to continue...";
+
+    int a = 4;
+    int b = 2;
+    int c = 3;
+
+    int sum = a + b + c;
+    int mult = a * b * c;
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << mult;
     return 0;
+
 }
